ac22122b23f08ae9882d89854cc5546af8e439ee
Florian Le Moal <florian@MacBook-Pro-de-Florian.local>
2020-09-25 12:39:51 +0200

    second push

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
+import {Router} from 'react-router';
+import Room from './containers/Room';
 import './App.css';
 
 function App() {
@@ -10,6 +12,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <Router path="/"><Room/></Router>
         <a
           className="App-link"
           href="https://reactjs.org"
