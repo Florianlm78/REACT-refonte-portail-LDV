61275dbd1ae61bf5652f3d6e33131c775bbfca37
Florian Le Moal <florian@macbook-pro-de-florian.home>
2020-10-04 23:42:25 +0200

    final

--- src/routes/jours/mardi.jsx
+++ src/routes/jours/mardi.jsx
@@ -13,21 +13,57 @@ class Mardi extends Component {
 
 
       return(
-          <div>
+        <section>
+          <h2>PULV - Amphi</h2>
+          <article>
+          
             {       
                     salles.MARDI[0].Amphi.map((salle) => {
                         return (
                           <div>
-                             
+                            {console.log(salles.MARDI[0])}
                             <h4>{salle.nom}</h4>
-
+                            <p>Matin</p>
+                            <svg width="15" height="15">
+                <circle cx="7.5" cy="7.5" r="7.5" fill={salle.matin ? "green" : "red"} />
+                
+            </svg>
+            
+            <p>Après-Midi</p>
+            <svg width="15" height="15">
+              <circle cx="7.5" cy="7.5" r="7.5" fill={salle.aprem ? "green" : "red"} />
+            </svg>
                           </div>
                         )
                     }
                     )
                 }
-          </div>
-          
+          </article>
+          <h2>ILV - IIM - Salle de cours</h2>
+          <article>
+            {
+                salles.MARDI[0].Salles.map((salle) => {
+                  return (
+                    <div>
+                      {console.log(salles.MARDI[0])}
+                      <h4>{salle.nom}</h4>
+                      <p>Matin</p>
+                            <svg width="15" height="15">
+                <circle cx="7.5" cy="7.5" r="7.5" fill={salle.matin ? "green" : "red"} />
+                
+            </svg>
+            
+            <p>Après-Midi</p>
+            <svg width="15" height="15">
+              <circle cx="7.5" cy="7.5" r="7.5" fill={salle.aprem ? "green" : "red"} />
+            </svg>
+                    </div>
+                  )
+              }
+              )
+            }
+          </article>
+        </section> 
       )
   }
       
