fe8d012e0cb231f021157cd2f4dfb49796834a66
Niveda <57499746+NivedaSou@users.noreply.github.com>
2020-10-04 18:53:48 +0200

    salles json

--- src/salles.js
+++ src/salles.js
@@ -1,32 +1,509 @@
 module.exports = 
 
 {
-"IIM": [
+"LUNDI": [
     {
-        nom : "salle 1",
-        disponibility : true
-    },
+       "Amphi":[
+        {
+           nom:"Amphi A",
+           matin:true,
+           aprem:false,  
+       },
+       {
+        nom:"Amphi B",
+        matin:true,
+        aprem:false,  
+        },
+        {
+        nom:"Amphi E",
+        matin:true,
+        aprem:false,  
+        },
+        {
+        nom:"Amphi G",
+        matin:true,
+        aprem:false,  
+        },
+    
+            ],
 
-    {
-        nom : "salle 2",
-        disponibility : true
-    },
+       "Salles":[
+        {
+            nom:"E 105",
+            matin:true,
+            aprem:false,  
+        },
+        {
+            nom:"E 106",
+            matin:true,
+            aprem:false,  
+        },
+        {
+            nom:"E 206",
+            matin:true,
+            aprem:false,  
+        },
+        {
+            nom:"E 207/A",
+            matin:true,
+            aprem:false,  
+        },
+        {
+            nom:"E 207/B",
+            matin:true,
+            aprem:false,  
+        },
+        {
+            nom:"E 209",
+            matin:true,
+            aprem:false,  
+        },
+        {
+            nom:"E 302",
+            matin:true,
+            aprem:false,  
+        },
+        {
+            nom:"E 407",
+            matin:true,
+            aprem:false,  
+        },
+        {
+            nom:"E 408",
+            matin:true,
+            aprem:false,  
+        },
+        {
+            nom:"E 558",
+            matin:true,
+            aprem:false,  
+        },
+        {
+            nom:"E 559",
+            matin:true,
+            aprem:false,  
+        }
 
-    {
-        nom : "salle 3",
-        disponibility : true
-    },
 
-    {
-        nom : "salle 4",
-        disponibility : true
-    },
+       ],
+    
+           
 
-    {
-        nom : "salle 5",
-        disponibility : true
-    },
+    }
+        
+    
+
    
 ],
+"MARDI": [
+    {
+       "Amphi":[
+        {
+           nom:"Amphi A",
+           matin:true,
+           aprem:true,  
+       },
+       {
+        nom:"Amphi B",
+        matin:true,
+        aprem:true,  
+        },
+        {
+        nom:"Amphi E",
+        matin:true,
+        aprem:false,  
+        },
+        {
+        nom:"Amphi G",
+        matin:false,
+        aprem:false,  
+        },
+    
+            ],
+
+       "Salles":[
+        {
+            nom:"E 105",
+            matin:false,
+            aprem:true,  
+        },
+        {
+            nom:"E 106",
+            matin:false,
+            aprem:false,  
+        },
+        {
+            nom:"E 206",
+            matin:true,
+            aprem:false,  
+        },
+        {
+            nom:"E 207/A",
+            matin:true,
+            aprem:false,  
+        },
+        {
+            nom:"E 207/B",
+            matin:false,
+            aprem:false,  
+        },
+        {
+            nom:"E 209",
+            matin:false,
+            aprem:true,  
+        },
+        {
+            nom:"E 302",
+            matin:true,
+            aprem:false,  
+        },
+        {
+            nom:"E 407",
+            matin:false,
+            aprem:false,  
+        },
+        {
+            nom:"E 408",
+            matin:false,
+            aprem:false,  
+        },
+        {
+            nom:"E 558",
+            matin:false,
+            aprem:false,  
+        },
+        {
+            nom:"E 559",
+            matin:true,
+            aprem:false,  
+        }
+
+
+       ],
+    
+           
+
+    }
+        
+    
+
+   
+],
+"MERCREDI": [
+    {
+       "Amphi":[
+        {
+           nom:"Amphi A",
+           matin:true,
+           aprem:true,  
+       },
+       {
+        nom:"Amphi B",
+        matin:true,
+        aprem:true,  
+        },
+        {
+        nom:"Amphi E",
+        matin:true,
+        aprem:true,  
+        },
+        {
+        nom:"Amphi G",
+        matin:false,
+        aprem:true,  
+        },
+    
+            ],
+
+       "Salles":[
+        {
+            nom:"E 105",
+            matin:true,
+            aprem:true,  
+        },
+        {
+            nom:"E 106",
+            matin:false,
+            aprem:true,  
+        },
+        {
+            nom:"E 206",
+            matin:true,
+            aprem:false,  
+        },
+        {
+            nom:"E 207/A",
+            matin:true,
+            aprem:false,  
+        },
+        {
+            nom:"E 207/B",
+            matin:true,
+            aprem:false,  
+        },
+        {
+            nom:"E 209",
+            matin:true,
+            aprem:false,  
+        },
+        {
+            nom:"E 302",
+            matin:true,
+            aprem:false,  
+        },
+        {
+            nom:"E 407",
+            matin:false,
+            aprem:false,  
+        },
+        {
+            nom:"E 408",
+            matin:true,
+            aprem:false,  
+        },
+        {
+            nom:"E 558",
+            matin:true,
+            aprem:true,  
+        },
+        {
+            nom:"E 559",
+            matin:true,
+            aprem:true,  
+        }
+
+
+       ],
+    
+           
+
+    }
+        
+    
+
+   
+],
+"JEUDI": [
+    {
+       "Amphi":[
+        {
+           nom:"Amphi A",
+           matin:true,
+           aprem:true,  
+       },
+       {
+        nom:"Amphi B",
+        matin:true,
+        aprem:false,  
+        },
+        {
+        nom:"Amphi E",
+        matin:true,
+        aprem:false,  
+        },
+        {
+        nom:"Amphi G",
+        matin:true,
+        aprem:false,  
+        },
+    
+            ],
+
+       "Salles":[
+        {
+            nom:"E 105",
+            matin:true,
+            aprem:false,  
+        },
+        {
+            nom:"E 106",
+            matin:true,
+            aprem:false,  
+        },
+        {
+            nom:"E 206",
+            matin:true,
+            aprem:false,  
+        },
+        {
+            nom:"E 207/A",
+            matin:true,
+            aprem:false,  
+        },
+        {
+            nom:"E 207/B",
+            matin:true,
+            aprem:false,  
+        },
+        {
+            nom:"E 209",
+            matin:true,
+            aprem:false,  
+        },
+        {
+            nom:"E 302",
+            matin:true,
+            aprem:false,  
+        },
+        {
+            nom:"E 407",
+            matin:true,
+            aprem:false,  
+        },
+        {
+            nom:"E 408",
+            matin:true,
+            aprem:false,  
+        },
+        {
+            nom:"E 558",
+            matin:true,
+            aprem:false,  
+        },
+        {
+            nom:"E 559",
+            matin:true,
+            aprem:false,  
+        }
+
+
+       ],
+    
+           
+
+    }
+        
+    
+
+   
+],
+"VENDREDI": [
+    {
+       "Amphi":[
+        {
+           nom:"Amphi A",
+           matin:true,
+           aprem:false,  
+       },
+       {
+        nom:"Amphi B",
+        matin:true,
+        aprem:false,  
+        },
+        {
+        nom:"Amphi E",
+        matin:true,
+        aprem:false,  
+        },
+        {
+        nom:"Amphi G",
+        matin:true,
+        aprem:false,  
+        },
+    
+            ],
+
+       "Salles":[
+        {
+            nom:"E 105",
+            matin:true,
+            aprem:false,  
+        },
+        {
+            nom:"E 106",
+            matin:true,
+            aprem:false,  
+        },
+        {
+            nom:"E 206",
+            matin:true,
+            aprem:false,  
+        },
+        {
+            nom:"E 207/A",
+            matin:true,
+            aprem:false,  
+        },
+        {
+            nom:"E 207/B",
+            matin:true,
+            aprem:false,  
+        },
+        {
+            nom:"E 209",
+            matin:true,
+            aprem:false,  
+        },
+        {
+            nom:"E 302",
+            matin:true,
+            aprem:false,  
+        },
+        {
+            nom:"E 407",
+            matin:true,
+            aprem:false,  
+        },
+        {
+            nom:"E 408",
+            matin:true,
+            aprem:false,  
+        },
+        {
+            nom:"E 558",
+            matin:true,
+            aprem:false,  
+        },
+        {
+            nom:"E 559",
+            matin:true,
+            aprem:false,  
+        }
+
+
+       ],
+    
+           
+
+    }
+        
+    
+
+   
+]
 
 }
+
+
+// module.exports = 
+
+// {
+// "IIM": [
+//     {
+//         nom : "salle 1",
+//         disponibility : true
+//     },
+
+//     {
+//         nom : "salle 2",
+//         disponibility : true
+//     },
+
+//     {
+//         nom : "salle 3",
+//         disponibility : true
+//     },
+
+//     {
+//         nom : "salle 4",
+//         disponibility : true
+//     },
+
+//     {
+//         nom : "salle 5",
+//         disponibility : true
+//     },
+   
+// ],
+
+// }
